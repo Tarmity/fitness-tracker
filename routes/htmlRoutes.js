98b818e9aeeff27bde3805a422a5e77944298b88
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-09-13 14:34:57 +1000

    updated the href links

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,15 +2,15 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "./../html/index"));
+    res.sendFile(path.join(__dirname + "./../html/index.html"));
 })
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "./../html/exercise"));
+    res.sendFile(path.join(__dirname + "./../html/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname +"./../html/stats"));
+    res.sendFile(path.join(__dirname +"./../html/stats.html"));
 });
 
 module.exports = router;
