2f663b24eb7be06ade9edf074596732550670824
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-09-13 13:36:23 +1000

    updated the Hfre path

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./../Views/index.html"));
+})
+
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname + "./../Views/exercise.html"));
 });
