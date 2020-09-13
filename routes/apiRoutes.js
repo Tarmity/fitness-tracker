6717fff49f38b3ef06244135d9072b5de5fb8c31
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-09-13 17:27:01 +1000

    delete old syntax

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,5 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
-// const path = require("path");
-
-// router.get("/exercise", (req, res) => {
-//     res.sendFile(path.join(__dirname + "./../exercise.html"));
-// }) 
-
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
